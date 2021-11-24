28ad8aab72b51c4815488a5bb22e9652ae088bca
phlgr <philipp@gartz.dev>
2021-11-24 12:45:03 +0100

    ✨ Create basic structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
+const exampleTodos = [
+  { name: 'Fische füttern', description: 'Dringend!' },
+  { name: 'Kochen', description: 'Nudeln' },
+];
+
 function App(): JSX.Element {
-  return <BrowserRouter></BrowserRouter>;
+  const [todos, setTodos] = useState(exampleTodos);
+  return (
+    <BrowserRouter>
+      <h1>Todo</h1>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Description" />
+        <input type="submit" />
+      </form>
+      {todos.map((todo) => (
+        <div>
+          <h3>{todo.name}</h3>
+          <p>{todo.description}</p>
+        </div>
+      ))}
+    </BrowserRouter>
+  );
 }
 
 export default App;
