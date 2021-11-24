57a69818eef4ffec18e06a66660071ee5f349e9d
phlgr <philipp@gartz.dev>
2021-11-24 12:45:46 +0100

    ✨ Save todos in State

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,12 +9,31 @@ const exampleTodos = [
 
 function App(): JSX.Element {
   const [todos, setTodos] = useState(exampleTodos);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const newTodo = { name, description };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <BrowserRouter>
       <h1>Todo</h1>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Description" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
         <input type="submit" />
       </form>
       {todos.map((todo) => (
